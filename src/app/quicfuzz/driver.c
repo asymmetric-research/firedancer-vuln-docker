f250e55e94769e1cf491205b81fa92782aae16c9
Valerio <valerio@asymmetric.re>
2025-10-13 12:36:42 +0200

    comments and polisg

--- src/app/quicfuzz/driver.c
+++ src/app/quicfuzz/driver.c
@@ -1,9 +1,9 @@
 #include "driver.h"
 #include "../../disco/metrics/fd_metrics.h"
 #include "../../disco/net/fd_net_tile.h" /* fd_topos_net_tiles */
-#include "../../disco/net/fd_net_tile.h"
 #include "../fdctl/config.h"
 #include "../fdctl/topology.h"
+#include <assert.h>
 
 extern fd_topo_run_tile_t fd_tile_quic;
 extern fd_topo_run_tile_t fd_tile_verify;
@@ -124,40 +124,53 @@ isolated_quic_topo( fd_drv_t * drv, fd_topo_obj_callbacks_t * callbacks[]  ) {
 	fd_config_t * config = &drv->config;
 
   fd_topo_t * topo = &config->topo;
-  ulong tile_to_cpu[ FD_TILE_MAX ] = {0};
+  ulong tile_to_cpu[ FD_TILE_MAX ] = {0}; // same cpu0 for testing
   fd_topob_new( &config->topo, config->name );
 
   ulong quic_tile_cnt   = 1; 
   ulong net_tile_cnt    = 1;  
 		
-	fd_topob_wksp( topo, "quic"         );
-  fd_topob_wksp( topo, "metric_in"    );	
-	fd_topob_wksp( topo, "quic_net"    );
 
+  fd_topob_wksp( topo, "metric_in"    );	
 
 
 #define FOR(cnt) for( ulong i=0UL; i<cnt; i++ )  
-  FD_LOG_INFO(("provider %s", config->net.provider));
-  // add sock tile
-  fd_topos_net_tiles( topo, net_tile_cnt, &config->net, 0, 0, 0, tile_to_cpu );
-// FOR(net_tile_cnt) fd_topos_net_rx_link( topo, "sock_quic",   i, config->net.ingress_buffer_size );
-FOR(net_tile_cnt) fd_topob_link( topo, "net_quic", "net_umem", config->net.ingress_buffer_size, FD_NET_MTU, 64 );
-FOR(net_tile_cnt)  fd_topob_tile_out( topo, "sock", 0, "net_quic", 0 );
-FOR(quic_tile_cnt)   fd_topob_tile( topo, "quic",    "quic",    "metric_in",  0, 0,        0 );
-FOR(quic_tile_cnt) fd_topob_link( topo, "quic_net",     "quic_net", config->net.ingress_buffer_size, FD_NET_MTU,64UL );
+
+
+  assert(strcmp("socket",config->net.provider) == 0);
+  /**
+   * FD helper for creating:
+   * - wksp net_umem and sock
+   * - sock tile
+   */
+  fd_topos_net_tiles( topo, net_tile_cnt, &config->net, 1, 1, 1, tile_to_cpu );
+  fd_topob_wksp( topo, "quic");
+FOR(quic_tile_cnt)   fd_topob_tile( topo, "quic","quic","metric_in",  0, 0,0 );
+FOR(quic_tile_cnt) fd_topob_link( topo, "quic_net", "quic", config->net.ingress_buffer_size, FD_NET_MTU, 64 );
 FOR(quic_tile_cnt) fd_topob_tile_out(    topo, "quic",i,"quic_net",  i);
-FOR(quic_tile_cnt) for( ulong j=0UL; j<net_tile_cnt; j++ ) fd_topob_tile_in(topo, "quic",i,"metric_in", "net_quic",j,FD_TOPOB_UNRELIABLE, FD_TOPOB_POLLED ); 
-FOR(quic_tile_cnt) fd_topob_tile_in( topo, "sock", 0, "metric_in", "quic_net", 0, FD_TOPOB_UNRELIABLE, FD_TOPOB_POLLED );
+/**
+ * adds `quic_net` as link in to tile `sock`
+ */
+FOR(quic_tile_cnt)  fd_topos_tile_in_net( topo,"metric_in", "quic_net",i,FD_TOPOB_UNRELIABLE, FD_TOPOB_POLLED );
+
+/**
+ * creates link `net_quic` in wksp `net_umem` 
+ * adds `net_quic` as tile `sock` out
+ */
+FOR(net_tile_cnt) fd_topos_net_rx_link( topo, "net_quic",   i, config->net.ingress_buffer_size );
+FOR(net_tile_cnt) fd_topob_tile_in( topo, "quic", 0UL, "metric_in", "net_quic", 0UL, FD_TOPOB_UNRELIABLE, FD_TOPOB_POLLED );
+
+//quic link out to verify - A CONSUMER TILE IS REQUIRED
+// FOR(quic_tile_cnt) fd_topob_link( topo, "quic_verify", "quic", config->net.ingress_buffer_size, FD_NET_MTU, 64 );
+// FOR(quic_tile_cnt) fd_topob_tile_out(    topo, "quic",i,"quic_verify",  i);
+
 
   for( ulong i=0UL; i<topo->tile_cnt; i++ ) fd_topo_configure_tile( &topo->tiles[ i ], config );
-
-  // fd_topo_obj_t * poh_shred_obj = fd_topob_obj( topo, "dcache", "net_umem" );
-  // fd_topo_tile_t* sock_tile = find_topo_tile(drv, "sock");
-  // fd_topob_tile_uses( topo, sock_tile, poh_shred_obj, FD_SHMEM_JOIN_MODE_READ_WRITE );
-  
+FOR(net_tile_cnt) fd_topos_net_tile_finish( topo, i );  
+  fd_topob_auto_layout( topo, 0 );
   (void)find_topo_tile;
   fd_topob_finish( topo, callbacks );
-	config->topo = *topo;	
+  fd_topo_print_log( /* stdout */ 1, topo );
 }
 
 void
