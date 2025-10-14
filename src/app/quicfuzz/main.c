4b4b3c654e36661f8dabd0543ec8ead2eee9b601
Valerio <valerio@asymmetric.re>
2025-10-14 15:17:52 +0200

    firestarter debug

--- src/app/quicfuzz/main.c
+++ src/app/quicfuzz/main.c
@@ -46,14 +46,18 @@ extern uchar const fdquic_default_config[];
 extern ulong const fdquic_default_config_sz;
 FD_IMPORT_BINARY( fdquic_default_config, "src/app/quicfuzz/config/default.toml" );
 
+extern uchar const fdctl_default_config[];
+extern ulong const fdctl_default_config_sz;
+FD_IMPORT_BINARY( fdctl_default_config, "src/app/fdctl/config/default.toml" );
+
+
 action_t * ACTIONS[] = { NULL };
 
 
 int
 main( int    argc,
       char** argv ) {
-    (void)argc;
-    (void)argv;
+    FD_LOG_INFO(("START"));
 
     void * shmem = aligned_alloc( fd_drv_align(), fd_drv_footprint() );
     if( FD_UNLIKELY( !shmem ) ) FD_LOG_ERR(( "malloc failed" ));
@@ -68,12 +72,13 @@ main( int    argc,
     char * user_config = NULL;
     ulong user_config_sz = 0UL;
     if( FD_LIKELY( opt_user_config_path ) ) {
+      FD_LOG_INFO(("USER CONFIG"));
       user_config = fd_file_util_read_all( opt_user_config_path, &user_config_sz );
       if( FD_UNLIKELY( user_config==MAP_FAILED ) ) FD_LOG_ERR(( "failed to read user config file `%s` (%d-%s)", opt_user_config_path, errno, fd_io_strerror( errno ) ));
       fd_config_load( 0, 0, 1, (char const *)fdquic_default_config, fdquic_default_config_sz, NULL, NULL, 0UL, user_config, user_config_sz, opt_user_config_path, &drv->config );      
     } else {
       fd_config_load( 0, 0, 1, (char const *)fdquic_default_config, fdquic_default_config_sz, NULL, NULL, 0UL, NULL, 0UL, NULL, &drv->config );
-    }    
+    }
 
 
     FD_LOG_INFO(("NAME %s", drv->config.name ));
